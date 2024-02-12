f19d4c62835882c6fa1252fca4c2b36979aacb3f
Yazid Bintang <yazidbintang2711@gmail.com>
2024-02-12 22:54:35 +0700

    Add gameUiState

---
+++ app/src/main/java/com/latihan/unscramble/ui/theme/GameUiState.kt
@@ -0,0 +1,9 @@
+package com.latihan.unscramble.ui.theme
+
+data class GameUiState(
+    val currentScrambleWord: String = "",
+    val currentWordCount: Int = 1,
+    val score: Int = 0,
+    val isGuessedWordWrong: Boolean = false,
+    val isGameOver: Boolean = false
+)
